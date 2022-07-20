70c5b9731eb2a9f3fca70b447c670c7837320d6c
Alyssarko <alyssako23@fsha.org>
2022-07-20 13:48:24 -0700

    edited table

---
+++ Group A5 AvoCardio/ViewTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewTableViewController.swift
+//  Group A5 AvoCardio
+//
+//  Created by Alyssa Ko '23 on 7/20/22.
+//
+
+import UIKit
+
+class ViewTableViewController: UIViewController {
+    @Outlet weak var table: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
